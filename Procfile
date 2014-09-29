4785011590831973d94b4a59cc839d4208acff06
Nathan Feger <nfeger@spredfast.com>
2014-09-28 21:00:41 -0500

    more working getting kotlin to work.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:    java $JAVA_OPTS -cp target/classes:target/dependency/* MainKotlin
+web:    java $JAVA_OPTS -cp target/classes:target/dependency/* Runner
---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+/**
+ * Created by nfeger on 9/28/14.
+ */
+public class Runner {
+    public static void main(String...args) {
+//        MainKotlin.object$.main(args);
+        new MainKotlin().main(args);
+    }
+}
